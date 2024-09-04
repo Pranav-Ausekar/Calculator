ada0e7fdf4cb84854a83d5cecb537b8891bf73fd
Pranav Ausekar <pranavausekar1@gmail.om>
2024-09-04 11:32:48 +0530

    new calculator project files to be uploaded

---
+++ script.js
@@ -0,0 +1,28 @@
+let clickedButtons = document.querySelectorAll('.buttons');
+let string = " ";
+
+Array.from(clickedButtons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+
+        else if (e.target.innerHTML == "Sq") {
+            string = string * string;
+            document.querySelector('input').value = string;
+        }
+
+        else if (e.target.innerHTML == 'AC') {
+            string = " ";
+            document.querySelector('input').value = string;
+        }
+
+        else {
+            console.log(e.target);
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+    })
+});
